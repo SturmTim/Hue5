d49a5aa0c8c9e332aa334fb8e706bf5045d1ad08
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2020-10-30 23:57:42 +0100

    readSudoku

--- Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
+++ Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -1,6 +1,11 @@
 package net.htlgrieskirchen.pos3.sudoku;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /* Please enter here an answer to task four between the tags:
  * <answerTask4>
@@ -8,14 +13,40 @@ import java.io.File;
  * </answerTask4>
  */
 public class SudokuSolver implements ISodukoSolver {
+
+    int[][] sudoku = new int[9][9];
+
     public SudokuSolver() {
         //initialize if necessary
     }
 
     @Override
     public final int[][] readSudoku(File file) {
-        // implement this method
-        return new int[0][0]; // delete this line!
+        AtomicInteger j = new AtomicInteger();
+
+        try {
+            if (!file.canRead() || !file.isFile()) {
+                System.out.println("ERROR!");
+                System.exit(0);
+            } else {
+
+                Files.lines(file.toPath())
+                        .forEach(string -> {
+                            String[] nStrings = string.split(";");
+
+                            for (int i = 0; i < sudoku.length; i++) {
+                                sudoku[j.intValue()][i] = Integer.parseInt(nStrings[i]);
+
+                            }
+                            j.addAndGet(1);
+
+                        });
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(SudokuSolver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sudoku;
     }
 
     @Override
@@ -29,7 +60,7 @@ public class SudokuSolver implements ISodukoSolver {
         // implement this method
         return new int[0][0]; // delete this line!
     }
-    
+
     @Override
     public int[][] solveSudokuParallel(int[][] rawSudoku) {
         // implement this method
